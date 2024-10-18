e13680e1bc5c55866527c74541600fcd445c3766
Crissper1 <Crissper11@gmail.com>
2024-10-18 16:23:40 -0300

    p

---
+++ ConnectBDjava/IniSesion/src/ventanaMensaje.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class ventanaMensaje extends ventanaBase {
+    private JComboBox comboContactos;
+    private JLabel aId;
+    private JTextArea textArea1;
+    private JButton enviarButton;
+    private JPanel JpanelMensaje;
+
+    public ventanaMensaje(String titulo) {
+        super("Mensaje");
+    }
+}
